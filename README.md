3ebbc9bffc68501c40cdf13885ff7f9570e6b655
Radoslav Husar <radosoft@gmail.com>
2016-04-05 14:54:54 +0200

    Upgrade example to WildFly 10.0.0.Final

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Random load metric example.
 Usage:
 
 	$ mvn clean install
-	$ cd etc
-	$ ./patch.sh /path/to/wildfly
+	$ cd etc/
+	$ ./patch.sh /path/to/wildfly10
 
 Enjoy.
 
--- etc/patch.sh
+++ etc/patch.sh
@@ -3,18 +3,21 @@
 if [ "$1x" == "x" ]
 then
 	echo "Usage:"
-	echo "$0 /path/to/wildfly"
+	echo "$0 /path/to/wildfly10"
 	exit
 else
 	echo Patching: $1..
 
 	echo Copying metrics jar..
-	cp ../target/mod_cluster-custom-metric-example-1.0-SNAPSHOT.jar $1/modules/system/layers/base/org/jboss/mod_cluster/core/main/
+	cp ../target/mod_cluster-custom-load-metric-example-1.0.0.Final-SNAPSHOT.jar $1/modules/system/layers/base/org/jboss/mod_cluster/core/main/
 
-	echo Patching module.xml..
+    echo Backing up module.xml..
+    cp $1/modules/system/layers/base/org/jboss/mod_cluster/core/main/module.xml $1/modules/system/layers/base/org/jboss/mod_cluster/core/main/module.xml.original
+
+	echo Overwriting module.xml..
 	cp module.xml $1/modules/system/layers/base/org/jboss/mod_cluster/core/main/
 
-	echo Patching standalone-ha.xml..
+	echo Overwriting standalone-ha.xml..
 	cp standalone-ha.xml $1/standalone/configuration/
 
 	echo Done.
